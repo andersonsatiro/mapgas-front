0c0677474aaa07dd683016b0c82bd5f304abf421
anderson.paiva <anderson.paiva@quark.tec.br>
2025-01-23 21:57:22 -0300

    [andersonsatiro] - MG-2 - criar section do título principal com informações sobre o Mapgas

---
+++ src/app/home/main-title/main-title.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-title',
+  imports: [],
+  templateUrl: './main-title.component.html',
+  styleUrl: './main-title.component.scss'
+})
+export class MainTitleComponent {
+
+}
